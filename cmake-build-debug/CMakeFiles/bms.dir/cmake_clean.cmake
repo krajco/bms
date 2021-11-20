7b0be1139fd3d4ce0619995e5b70cc01ba4f1b78
krajco <xkrajc17@stud.fit.vutbr.cz>
2021-11-20 18:33:55 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/bms.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bms.dir/RSC.cpp.o"
+  "CMakeFiles/bms.dir/global.cpp.o"
+  "CMakeFiles/bms.dir/main.cpp.o"
+  "bms.exe"
+  "bms.pdb"
+  "libbms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
